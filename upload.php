105e69949af510b05a216469654b84a3f216e482
ADMIN <morrison0899@gmail.com>
2017-04-07 19:53:58 +0100

    fifth commit

--- upload.php
+++ upload.php
@@ -3,6 +3,37 @@
 if(isset($_POST['submit'])){
     $file = $_FILES['file'];
     print_r($file);
+
     $fileName=$_FILES['file']['name'];
+    $fileTmpName=$_FILES['file']['tmp_name'];
+    $fileSize=$_FILES['file']['size'];
+    $fileError=$_FILES['file']['error'];
+    $fileType=$_FILES['file']['type'];
+
+    $fileExt=explode('.',$fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg','jpeg','png','mp3');
+
+    if(in_array($fileActualExt,$allowed)){
+            if($fileError===0){
+                if(filesize<100000){
+                    $fileNameNew = uniqid('',true).".".$fileActualExt;
+                    $fileDestination='uploads/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName,$fileDestination);
+                    header("Location:index.php");
+                }else{
+                    echo "File too big";
+                }
+            }else{
+                echo "Something went wrong with your file";
+            }
+
+
+    }else{
+        echo "you canot upload files of this type";
+    }
+
+
 
 }
