295354fb582d6df7f14c62399faedbf8a0ee1e4e
ADMIN <morrison0899@gmail.com>
2017-04-09 15:19:08 +0100

    fifth commit

--- upload.php
+++ upload.php
@@ -11,7 +11,7 @@ if(isset($_POST['submit'])){
     $allowed = array('jpg','jpeg','png','mp3');
     if(in_array($fileActualExt,$allowed)){
         if($fileError===0){
-            if($fileSize<100000){
+            if($fileSize<1000000){
                 $fileNameNew = uniqid('',true).".".$fileActualExt;
                 $fileDestination='uploads/'.$fileNameNew;
                 move_uploaded_file($fileTmpName,$fileDestination);
