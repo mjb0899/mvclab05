d1966b61d7dfb98505c517d4d76680a5fff716ab
ADMIN <morrison0899@gmail.com>
2017-04-14 18:35:37 +0100

    fifth commit

--- insertRating.php
+++ insertRating.php
@@ -12,25 +12,23 @@
  */
 
 include ("dbConnect.php");
-
-
+$ipaddress=1;
 
 if (isset($_POST['rate']) && !empty($_POST['rate'])) {
 
-    $rate = $conn->real_escape_string($_POST['rate']);
+    $rate = $db->real_escape_string($_POST['rate']);
+
 // check if user has already rated
-    $sql = "SELECT `id` FROM `tbl_rating` WHERE `user_id`='" . $ipaddress . "'";
-    $result = $conn->query($sql);
+    $sql = "SELECT id FROM tbl_rating WHERE user_id='" . $ipaddress . "'";
+    $result = $db->query($sql);
     $row = $result->fetch_assoc();
     if ($result->num_rows > 0) {
         echo $row['id'];
     } else {
 
-        $sql = "INSERT INTO `tbl_rating` ( `rate`, `user_id`) VALUES ('" . $rate . "', '" . $ipaddress . "'); ";
-        if (mysqli_query($conn, $sql)) {
+        $sql = "INSERT INTO tbl_rating ( rate, user_id) VALUES ('" . $rate . "', '" . $ipaddress . "'); ";
+        if (mysqli_query($db, $sql)) {
             echo "0";
         }
     }
 }
-$conn->close();
-?>
