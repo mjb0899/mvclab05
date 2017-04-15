c3451f0357c43c0157ef8b2691b424d0dc006afa
ADMIN <morrison0899@gmail.com>
2017-04-15 21:32:18 +0100

    fifth commit

--- rating.php
+++ rating.php
@@ -23,7 +23,7 @@ if (isset($_GET['rate']) && !empty($_GET['rate'])) {
     $rate = $db->real_escape_string($_GET['rate']);
 
 // check if user has already rated
-    $sql = "SELECT id FROM tbl_rating WHERE user_id='" . $ipaddress . "'";
+    $sql = "SELECT id FROM tbl_rating WHERE user_id='" . $id . "'";
     $result = $db->query($sql);
     $row = $result->fetch_assoc();
     if ($result->num_rows > 0) {
