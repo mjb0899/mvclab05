fb3c29e5d653aafcafaaab857fa8281d53c0c1fc
ADMIN <morrison0899@gmail.com>
2017-04-17 01:41:08 +0100

    fifth commit

--- insertSuperhero.php
+++ insertSuperhero.php
@@ -5,15 +5,7 @@
  * Date: 14/03/2017
  * Time: 13:39
  */
-include("dbConnect.php");
 $firstname = $_POST["fname"];
 $lastname= $_POST["lname"];
 $superpower = $_POST["power"];
-
-$sql= "INSERT INTO superheros(firstname,lastname,mainSuperpower)VALUES ('$firstname','$lastname','$superpower')";
-if(mysqli_query($db,$sql)){
-}
-else{
-    echo "Error:".$sql."<br>".mysqli_error($db);
-}
 echo "->".$superpower."->".$firstname."->".$lastname;
