e874376dc84c1f6db486dc0818fb6fffb0653c87
ADMIN <morrison0899@gmail.com>
2017-04-21 19:53:05 +0100

    fifth commit

--- reply.php
+++ reply.php
@@ -5,8 +5,8 @@
  * Date: 17/04/2017
  * Time: 00:33
  */
-$name=$_GET['name'];
-$san= mysqli_real_escape_string($db,$name);
+$name=$_POST['name'];
+$san= mysqli_real_escape_string($db,$_POST['name']);
 echo $san;
 
 
